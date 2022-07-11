82d0642ae2796361c6335ec6e7fea19e98d24b88
Rachael <themetalsaint@gmail.com>
2022-07-11 13:33:23 -0400

    fixed parenthesis

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const PORT = 3001;
 const app = express();
 
-app.get('/'), (req, res) => {
-    res.send('Hellow World')
-}
+app.get('/', (req, res) => {
+    res.send('Hellow World');
+})
 
 app.listen(PORT, ()=>{
     console.log('listening on port'+PORT )
 } )
+
