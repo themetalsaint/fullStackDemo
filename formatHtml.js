1ab1a45c103f9f427016496c719e903ceecea554
Rachael <themetalsaint@gmail.com>
2022-07-12 13:15:22 -0400

    added script and fixed typos

--- formatHtml.js
+++ formatHtml.js
@@ -1,18 +1,20 @@
 formatHtml = () => {
-    return 
-        `
+    return `
         <html>
             <head>
                 <title>Story Writer</title>
             </head>
                 <body>
                     <h1>Story Writer</h1>
-                
+                    <p>Story Goes here</p>
+                    <input id="word">
+                    <button id="add">Add</button>
                 </body>
             
+                <script src="script.js"></script>
 
         </html>
-        `
+      `
 }
 
-module.export = {formatHtml}
+module.exports = {formatHtml}
--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const PORT = 3001;
 
 const app = express();
 
+app.use(express.static("public"));
+
 app.get('/', (req, res) => {
-    res.send(formatHtml);
+    res.send(formatHtml());
 })
 
 app.listen(PORT, ()=>{
